cc2ffeb283391c6217759370b0c38b67cd392893
blurryfacebeat <blurryfacebeat@gmail.com>
2020-12-23 22:59:04 +0700

    setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './App.scss';
+import character from './assets/character.svg';
+import questions from './questions.json';
 
 function App() {
+  const [sentence, setSentence] = useState(questions[0].sentence);
+  const [blocks, setBlocks] = useState(questions[0].blocks);
+  const [useBlocks, setUseBlocks] = useState([]);
+
+  useEffect(() => {
+    console.log(sentence);
+    console.log(blocks);
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <h1 className="app-header">Переведите это предложение</h1>
+      <div className="sentence-contaier">
+        <img src={character} alt="Инопришеленец" className="app-character" />
+        <div className="sentence">
+          {sentence.map(s => (
+            <span
+              className="sentence-item"
+              key={s.id}
+            >
+              {s.text}
+            </span>
+          ))}
+
+        </div>
+      </div>
+      <pre>{}</pre>
     </div>
   );
 }
